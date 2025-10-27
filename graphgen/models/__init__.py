ba48ee53a062ed4d0a776fad2d093c23e0b8dce8
chenzihong-gavin <chenzihong_gavin@foxmail.com>
2025-10-27 11:58:35 +0800

    chore: delete duplicate model

--- graphgen/models/__init__.py
+++ graphgen/models/__init__.py
@@ -8,7 +8,6 @@ from .generator import (
 )
 from .kg_builder import LightRAGKGBuilder, MMKGBuilder
 from .llm.openai_client import OpenAIClient
-from .llm.topk_token_model import TopkTokenModel
 from .partitioner import (
     AnchorBFSPartitioner,
     BFSPartitioner,
