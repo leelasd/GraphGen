c4f69b5616d95f7ee3382c36a4a81b815326c1ef
chenzihong-gavin <chenzihong_gavin@foxmail.com>
2025-11-07 18:22:58 +0800

    fix: fix txt_reader, not splitting lines

--- graphgen/models/reader/txt_reader.py
+++ graphgen/models/reader/txt_reader.py
@@ -5,10 +5,6 @@ from graphgen.bases.base_reader import BaseReader
 
 class TXTReader(BaseReader):
     def read(self, file_path: str) -> List[Dict[str, Any]]:
-        docs = []
         with open(file_path, "r", encoding="utf-8") as f:
-            for line in f:
-                line = line.strip()
-                if line:
-                    docs.append({self.text_column: line})
+            docs = [{"type": "text", self.text_column: f.read()}]
         return self.filter(docs)
